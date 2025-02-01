cea53d24fabd8c79d99355f705d47a79c27dfcf1
若林将輝 <masaki.wakabaya@gmail.com>
2025-02-01 21:56:16 +0900

    update sample page

--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -1,11 +1,12 @@
 
 <script setup lang="ts">
-
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <template>
   <div class="sample">
     <h1>This is an sample page</h1>
+    <HelloWorld msg="こんにちは" />
   </div>
 </template>
 
