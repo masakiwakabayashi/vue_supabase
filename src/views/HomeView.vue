137b69699555ac76ef902fdb99fb3ab4bf85e00a
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-02-21 01:54:04 +0900

    update top page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import { supabase } from '@/lib/supabaseClient';
 import { onMounted, ref } from 'vue';
 
 const user = ref(null);
+const memos = ref(null);
 
 const getCurrentUser = async () => {
   const { data, error } = await supabase.auth.getUser();
@@ -13,13 +14,34 @@ const getCurrentUser = async () => {
   user.value = data.user;
 }
 
-onMounted(getCurrentUser);
+const getMemos = async () => {
+  const { data } = await supabase
+    .from('memos')
+    .select('*');
+
+  console.log(data);
+  memos.value = data;
+}
+
+onMounted(()=>{
+  getCurrentUser();
+  getMemos();
+});
 
 </script>
 
 <template>
-  <div>
-    <h1 class="p-3">Top Page</h1>
-    <p>{{ user?.email }}</p>
+  <div class="max-w-2xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
+    <h1 class="text-2xl font-bold text-gray-800 p-3 border-b border-gray-300">Top Page</h1>
+    
+    <p class="text-gray-700 mt-4">{{ user?.email }}</p>
+    
+    <div class="mt-6">
+      <h3 class="text-lg font-semibold text-gray-800 border-b border-gray-300 pb-2">Memos</h3>
+      <div v-for="memo in memos" :key="memo.id" class="bg-white p-4 mt-4 shadow-sm rounded-md border border-gray-200">
+        <p class="text-lg font-medium text-gray-900">{{ memo.title }}</p>
+        <p class="text-gray-700 mt-1">{{ memo.content }}</p>
+      </div>
+    </div>
   </div>
 </template>
