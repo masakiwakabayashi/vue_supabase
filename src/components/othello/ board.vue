0299f5a2963a3968c437b06023001a0fa0dc6fa2
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-02-26 23:08:22 +0900

    set sample stone

--- src/components/othello/ board.vue
+++ src/components/othello/ board.vue
@@ -6,13 +6,18 @@
 <template>
   <div>
     <div 
-      v-for="i in 8"
+      v-for="n in 8"
       class="flex"
     >
       <div
         v-for="i in 8"
         class="w-[100px] h-[100px] bg-green-400 border border-black"
-      ></div>
+      >
+        <div v-if="n == 4 && i == 4" >⚫️</div>
+        <div v-if="n == 4 && i == 5" >⚫️</div>
+        <div v-if="n == 5 && i == 4" >⚫️</div>
+        <div v-if="n == 5 && i == 5" >⚫️</div>
+      </div>
     </div>
   </div>
 </template>
