1567ed92d878bf7c6d975edd371887b2a91d1545
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-03-02 21:16:49 +0900

    update othello board

--- src/components/othello/board.vue
+++ src/components/othello/board.vue
@@ -4,10 +4,22 @@ import { ref } from 'vue';
 // 8×8のグリッドを初期化
 const grid = ref(Array.from({ length: 8 }, () => Array(8).fill("")));
 
-// クリックされたマスの値を変更（⚪️ ⇄ ⚫️）
-const updateCell = (y, x) => {
-  grid.value[y][x] = grid.value[y][x] === "⚪️" ? "⚫️" : "⚪️";
+const player = ref({
+  name: "",
+  color: "white"
+});
+
+//　石をセット
+const setCell = (y, x) => {
+  if (grid.value[y][x] === "") {
+    grid.value[y][x] = player.value.color  === "white" ? "⚪️" : "⚫️";
+  }
 };
+
+const changePlayerColor = () => {
+  // プレイヤーの色を交互に切り替える
+  player.value.color = player.value.color === "white" ? "black" : "white";
+}
 
 </script>
 
@@ -21,7 +33,10 @@ const updateCell = (y, x) => {
         v-for="(cell, x) in row"
         class="w-[100px] h-[100px] bg-green-400 border border-black flex items-center justify-center"
       >
-        <div @click="updateCell(y, x)" class="w-full h-full flex items-center justify-center">
+        <div @click="()=>{
+          setCell(y, x);
+          changePlayerColor();
+        }" class="w-full h-full flex items-center justify-center">
           <span class="text-6xl">{{ cell }}</span>
         </div>
       </div>
